934f9e471d62f99f4953b2c97c2d596486a3a52a
Mario Zelaya <mariozelaya23@gmail.com>
2022-04-06 14:33:05 -0700

    register and store classes

--- web/app.py
+++ web/app.py
@@ -1,12 +1,95 @@
 """
 Registration of a User
 Each user gets 10 tokens
-Retrieve his stored sentece on out database for 1 token
+Retrieve his stored sentence on out database for 1 token
 Retrieve his stored sentence on out database for 1 token
 """
 
+from flask import Flask, jsonify, request
+from flask_restful import Api, Resource
+from pymongo import MongoClient
+import bcrypt
+
+app = Flask(__name__)
+api = Api(app)
+
+client = MongoClient("mongodb://db:27017")
+db = client.SentencesDatabase
+users = db["Users"]
+
+class Register(Resource):
+    def post(self):
+        #Step 1 is to get posted data by the users
+        postedData = request.get_json()
+
+        #Get the data
+        username = postedData["username"]
+        password = postedData["password"]
+
+        hashed_pw = bcrypt.hashpw(password, bcrypt.gensalt())
+
+        #Store username and pw into the Database
+        users.insert({
+            "Username": username,
+            "Password": hashed_pw,
+            "Sentence": "",
+            "Tokens": 5
+        })
+
+        retJson = {
+            "status": 200,
+            "msg": "You successfully signed up for the API"
+        }
+        return jsonify(retJson)
+
+class Store(Resource):
+    def post(self):
+        #Step 1 get the posted data
+        postedData = request.get_json()
+
+        #Step 2 is to read the data
+        username = postedData["username"]
+        password = postedData["password"]
+        sentence = postedData["sentence"]
+
+        #Step 3 verify the username pw match
+        correct_pw = verifyPW(username, password)
+
+        if not correct_pw:
+            retJson = {
+                "Status": 302
+            }
+            return jsonify(retJson)
+
+        #Step 4 verify user has enough tokens
+        num_tokens = countTokens(username)
+        if num_tokens <= 0:
+            retJson = {
+                "status": 301
+            }
+            return jsonify(retJson)
+
+        #Step 5 store the sentence, take one token away and return 2000k
+        users.update({
+            "Username": username
+        }, {
+            "$set": {
+                "Sentence": sentence,
+                "Tokens": num_tokens - 1
+                }
+        })
+
+        retJson = {
+            "status": 200,
+            "msg": "Sentence saved successfully"
+        }
+        return jsonify(retJson)
 
 
+api.add_resource(Register, '/register')
+
+if __name__=="__main__":
+    app.run(host='0.0.0.0')
 
 
 """
